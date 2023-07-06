214b5400fd636a63ff20be4f00d5eef9ce5895b0
Hari-Keshav-Rajesh <harikeshav.rajesh@gmail.com>
2023-07-06 19:45:06 +0530

    Added remove button on every table row

--- script.js
+++ script.js
@@ -34,17 +34,18 @@ const table = document.querySelector('.table');
 submitButton.addEventListener('click',(event)=>{
     
     if((form.readStatus.value !== 'Yes'&&form.readStatus.value!=='No')===true){
-        alert("Please enter appropriate values in the specified case");
+        alert("Please enter Yes or No in the specified case");
         event.preventDefault(); 
     }
     else{
         let book={};
         addBookToLibrary(book=new Book(form.title.value,form.author.value,form.pages.value,form.readStatus.value));
         table.innerHTML+=`<tr>
-                                <td>${form.title.value}</td>
-                                <td>${form.author.value}</td>
-                                <td>${form.pages.value}</td>
-                                <td>${form.readStatus.value}</td>
+                                <td>${myLibrary[myLibrary.length-1].title}</td>
+                                <td>${myLibrary[myLibrary.length-1].author}</td>
+                                <td>${myLibrary[myLibrary.length-1].pages}</td>
+                                <td>${myLibrary[myLibrary.length-1].readStatus}</td>
+                                <td><button data-row="${row}" class="remove" onclick="remove(event)">Remove</button></td>
                             </tr>`
         event.preventDefault();
         form.title.value='';
@@ -52,6 +53,18 @@ submitButton.addEventListener('click',(event)=>{
         form.pages.value='';
         form.readStatus.value='';
         form.style.display = "none";
-    }
-
+        }
+        row+=1;
 });
+
+let row=0;
+
+function remove(event){
+   var td=event.target.parentNode;
+   var tr=td.parentNode;
+   tr.parentNode.removeChild(tr);
+   myLibrary.splice(document.querySelector(`[data-row="${row}"]`),1);
+}
+
+
+
